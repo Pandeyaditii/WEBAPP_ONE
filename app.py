725c64a84555aa683ac428b064ed521f0035634e
Aditi <pandeyaditi@gamil.com>
2023-07-17 13:42:05 +0530

    commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter, ImageDraw, ImageFont
 import os
 
 # create a folder images
@@ -38,4 +38,16 @@ if upload is not None:
         col2.image(img.filter(ImageFilter.SHARPEN), caption='Sharpen', use_column_width=True)
 
 
+    message = st.sidebar.text_input("Enter a message", value= "MONDAY")
+    font_size= st. sidebar.number_input("Enter font size", value=20)
+    font_color= st. sidebar.color_picker("select font color")
+    c1,c2 = st.sidebar.columns(2)
+    x=c1.number_input('X coordinate', value=img.width//2)
+    y=c2.number_input('Y coordinate',value=img.height//2)
 
+    #copy img to another
+    imgc = img.copy()
+    draw = ImageDraw.Draw(imgc)
+    font = ImageFont.truetype('myfont.ttf', font_size)
+    draw.text((x,y), message, font_color, font)
+    st.image(imgc, width=300)
